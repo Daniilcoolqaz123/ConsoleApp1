3f06c2c14d56420022e879277e375a01418f989e
Саша Пашкевич <pashkevich-nata@mail.ru>
2024-06-22 11:41:29 +0300

    Я изменил функцию +

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace PP21
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите A:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите B:");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите B:");
+            int C = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите D:");
+            int D = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите операцию: +, -, /, x");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "+":
+                    Console.WriteLine($"Результат сложения: {B + A}");
+
+                    Console.WriteLine($"Результат сложения: {A + B + C + C}");
+
+                    break;
+                case "-":
+                    Console.WriteLine($"Результат вычитания: {B - A}");
+                    break;
+                case "x":
+                    Console.WriteLine($"Результат умножения: {A * B}");
+                    break;
+                case "/":
+                    if (B != 0)
+                    {
+                        Console.WriteLine($"{A / (double)B}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: деление на ноль!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Ошибка: неверная операция!");
+                    break;
+            }
         }
     }
 }
