80d142c0ae1dd20a260fdb813cc7b4d78f8d5a12
Саша Пашкевич <pashkevich-nata@mail.ru>
2024-06-22 11:57:37 +0300

    Изм защита

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -33,7 +33,7 @@ namespace PP21
                     Console.WriteLine($"Результат вычитания: {A - B}");
                     break;
                 case "x":
-                    Console.WriteLine($"Результат умножения: {B * A}");
+                    Console.WriteLine($"Результат умножения: {A * B}");
                     break;
                 case "/":
                     if (B != 0)
